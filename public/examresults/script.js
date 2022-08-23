01bd067595fbdbb78675b15665d3647123800671
Savindu Supun <savindusupunsathsara@gmail.com>
2022-08-23 16:28:48 +0530

    :sparkles: database connected

--- public/examresults/script.js
+++ public/examresults/script.js
@@ -23,39 +23,112 @@ resetBtn.addEventListener('click', function (e) {
 });
 
 //function to get result
-function getResult(examIndexNo) {
+function getResultOriginal(examIndexNo) {
   const Url = `https://result.doenets.lk/result/service/AlResult?index=${examIndexNo}&nic=`;
   fetch(Url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      const { name, nic, districtRank, islandRank, zScore, stream } = data;
-      console.log(
-        'name : ' +
-          name +
-          ' nic : ' +
-          nic +
-          ' districtRank : ' +
-          districtRank +
-          ' islandRank : ' +
-          islandRank +
-          ' zScore : ' +
-          zScore +
-          ' stream : ' +
-          stream
-      );
-      document.getElementById('index').innerHTML = examIndexNo;
-      document.getElementById('name').innerHTML = name;
-      document.getElementById('nic').innerHTML = nic;
-      document.getElementById('Drank').innerHTML = districtRank;
-      document.getElementById('Irank').innerHTML = islandRank;
-      document.getElementById('zscore').innerHTML = zScore;
-      document.getElementById('stream').innerHTML = stream;
+      const {
+        year,
+        name,
+        nic,
+        districtRank,
+        islandRank,
+        zScore,
+        stream,
+        subjectResults,
+        examIndexNo,
+      } = data;
+      const sub1 = subjectResults[0].subjectName;
+      const sub1R = subjectResults[0].subjectResult;
+      const sub2 = subjectResults[1].subjectName;
+      const sub2R = subjectResults[1].subjectResult;
+      const sub3 = subjectResults[2].subjectName;
+      const sub3R = subjectResults[2].subjectResult;
+      const generalTest = subjectResults[3].subjectResult;
+      const english = subjectResults[4].subjectResult;
+
+      data = {
+        key: examIndexNo,
+        year,
+        name,
+        nic,
+        districtRank,
+        islandRank,
+        zScore,
+        stream,
+        sub1,
+        sub1R,
+        sub2,
+        sub2R,
+        sub3,
+        sub3R,
+        english,
+        generalTest,
+      };
+      injectData(data);
     })
     .catch((err) => {
       console.log(err);
     });
+}
+
+function getResult(examIndexNo) {
+  const url = `https://2l77ul.deta.dev/result/${examIndexNo}`;
+  fetch(url).then((response) => {
+    if (response.status === 200) {
+      response.json().then((data) => getResultOur(data));
+    } else {
+      console.log(response.status + ' ' + response.statusText);
+      getResultOriginal(examIndexNo);
+    }
+  });
+}
+
+function getResultOur(data) {
+  console.log(data);
+  injectData(data);
+}
+
+function injectData(data) {
+  const {
+    key,
+    year,
+    name,
+    nic,
+    districtRank,
+    islandRank,
+    zScore,
+    stream,
+    sub1,
+    sub1R,
+    sub2,
+    sub2R,
+    sub3,
+    sub3R,
+    english,
+    generalTest,
+  } = data;
+
+  document.getElementById('index').innerHTML = key;
+  document.getElementById('name').innerHTML = name;
+  document.getElementById('nic').innerHTML = nic;
+  document.getElementById('year').innerHTML = year;
+  document.getElementById('Drank').innerHTML = districtRank;
+  document.getElementById('Irank').innerHTML = islandRank;
+  document.getElementById('zscore').innerHTML = zScore;
+  document.getElementById('stream').innerHTML = stream;
+  document.getElementById('sub1').innerHTML = sub1;
+  document.getElementById('res1').innerHTML = sub1R;
+  document.getElementById('sub2').innerHTML = sub2;
+  document.getElementById('res2').innerHTML = sub2R;
+  document.getElementById('sub3').innerHTML = sub3;
+  document.getElementById('res3').innerHTML = sub3R;
+  document.getElementById('engres').innerHTML = english;
+  document.getElementById('cgtest').innerHTML = generalTest;
+
   setTimeout(() => {
     resultSheet.style.display = 'block';
-  }, 500);
+  }, 100);
 }
